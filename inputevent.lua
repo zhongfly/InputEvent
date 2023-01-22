natural-harmonia-gropius/input-event

Faster end to parsing conf lines starting with #

inputevent.lua
@@ -300,9 +300,9 @@ function bind_from_conf(path)
     local kv = {}
     for line in io.lines(path) do
         line = line:trim()
-        if line ~= "" then
+        if line ~= "" and line:sub(1, 1) ~= "#" then
             local key, cmd, comment = line:match("%s*([%S]+)%s+(.-)%s+#%s*(.-)%s*$")
-            if comment and key:sub(1, 1) ~= "#" then
+            if comment then
                 local comments = comment:split("#")
                 local events = table.filter(comments, function(i, v) return v:match("^@") end)
                 if events and #events > 0 then

zhongfly/InputEvent

(cherry picked from commit 7b0e4f9221e47f7ffacb8fcee73181157bc4f391)

inputevent.lua
@@ -456,9 +456,9 @@ local function read_conf(conf_path)
     local parsed = {}
     for line in io.lines(conf_path) do
         line = line:trim()
-        if line ~= "" then
+        if line ~= "" and line:sub(1, 1) ~= "#" then
             local key, cmd, comments = line:match("%s*([%S]+)%s+(.-)%s+#%s*(.-)%s*$")
-            if comments and key:sub(1, 1) ~= "#" then
+            if comments then
                 local comment = table.filter(comments:split("#"), comment_filter)
                 if comment and #comment > 0 then
                     local statement = comment[1]:match("^@(.*)"):trim()